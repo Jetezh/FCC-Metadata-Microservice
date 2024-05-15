640964a6d3712c645e098bd24c64db990c09c152
Jetezh 123 <vegasetiawan16@gmail.com>
2024-05-15 13:40:43 +0000

    completed

--- index.js
+++ index.js
@@ -2,20 +2,49 @@ require('dotenv').config();
 var express = require('express');
 var cors = require('cors');
 var app = express();
+var multer = require("multer");
+const path = require('path');
+const fs = require('fs');
+
+const uploadDir = 'uploads/';
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get("/api/fileanalyse", (req, res) => {
-  const { upfile } = req.body;
-});
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  const file = req.file;
 
+  if (file) {
+    res.json({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size
+    });
+  } else {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
